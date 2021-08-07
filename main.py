0875ff6594cb8ac0826e212bef35adbe6d5dd79c
daniel-wang-design <daniel.wang04@gmail.com>
2021-08-07 17:59:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import arcade
+
+width = 800
+height = 500
+title = "Game"
+
+class Main(arcade.Window):
+    """
+    Main game application class
+    """
+    def __init__(self):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BLUE)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    # main method
+    window = Main()
+    window.setup()
+    arcade.run()
+
+main()
+
