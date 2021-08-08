915d1a9ea4e16e08ccf3bdca32868e987cabab15
cindyhjli <cindy.hongjia.li16@gmail.com>
2021-08-07 22:01:30 -0400

    change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import arcade
 
-width = 800
-height = 500
+width = 1440
+height = 845
 title = "Game"
 
 class Main(arcade.Window):
@@ -10,13 +10,27 @@ class Main(arcade.Window):
     """
     def __init__(self):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.BLUE)
+        arcade.set_background_color(arcade.color.LIGHT_STEEL_BLUE)
 
     def setup(self):
-        pass
+
 
     def on_draw(self):
         arcade.start_render()
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        self.trash = arcade.get_sprites_at_point((x, y), self.sprites)
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        for trash in self.trash:
+            trash.center_x += dx
+            trash.center_y += dy
+
+    def on_mouse_release(self, x: float, y: float, button: int,
+                         modifiers: int):
+        if len(self.trash) == 0:
+            return
+        self.trash = []
 
 def main():
     # main method
